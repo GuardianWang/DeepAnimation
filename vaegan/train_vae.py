1db77b042d33191007555694961a8d6458059d21
guardianwang <962247283@qq.com>
2021-12-07 13:02:41 -0500

    set limit

--- vaegan/train_vae.py
+++ vaegan/train_vae.py
@@ -3,7 +3,9 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+            # tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.experimental.set_virtual_device_configuration(gpu, [
+                tf.config.experimental.VirtualDeviceConfiguration(memory_limit=9000)])
     except RuntimeError as e:
         print(e)
 
